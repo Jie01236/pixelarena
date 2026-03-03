6f34fa1164cb803835c7f866a2a3c5174c5fa842
Jie01236 <151651468+Jie01236@users.noreply.github.com>
2026-03-03 16:21:06 +0100

    assemblage

--- mfe-header/src/components/Navbar.jsx
+++ mfe-header/src/components/Navbar.jsx
@@ -7,9 +7,19 @@ function Navbar() {
   const [cartCount, setCartCount] = useState(0);
 
   useEffect(() => {
-    // TODO 1 : s'abonner à game:joined → incrémenter le badge notifications
-    // TODO 2 : s'abonner à cart:updated → mettre à jour le badge panier (count)
-    // TODO 3 : retourner le cleanup des 2 abonnements
+    // s'abonner à game:joined → incrémenter le badge notifications
+    const unsubscribeGameJoined = eventBus.on('game:joined', () => {
+      setNotifications(prev => prev + 1);
+    });
+    //s'abonner à cart:updated → mettre à jour le badge panier (count)
+    const unsubscribeCartUpdated = eventBus.on('cart:updated', (data) => {
+      setCartCount(data.count);
+    });
+    //retourner le cleanup des 2 abonnements
+    return () => {
+      unsubscribeGameJoined();
+      unsubscribeCartUpdated();
+    };
   }, []);
 
   return (
