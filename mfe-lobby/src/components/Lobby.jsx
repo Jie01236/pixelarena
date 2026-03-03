94f8e1fb064ea2723b47a785d6884542a520410d
Jie01236 <151651468+Jie01236@users.noreply.github.com>
2026-03-03 14:37:48 +0100

    catalog

--- mfe-lobby/src/components/Lobby.jsx
+++ mfe-lobby/src/components/Lobby.jsx
@@ -11,14 +11,8 @@ const GAMES = [
 
 function GameCard({ game }) {
   const handleJoinGame = () => {
-    // TODO 3: Emettre l'evenement 'game:joined' avec le nom de la partie
-    //
-    // Indice: eventBus.emit('nomEvenement', { cle: valeur })
-    //
-    // L'evenement doit contenir: { gameName: game.name }
-    //
-    // Ecrivez votre code ici:
-
+    // Emettre l'evenement 'game:joined' avec le nom de la partie
+    eventBus.emit('game:joined', { gameName: game.name });
 
     // Feedback visuel (gardez cette ligne)
     alert(`Vous avez rejoint : ${game.name}`);
